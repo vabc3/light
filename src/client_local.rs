3928882a8f33d9d25a35462663b1b5eb27e17536
kar <vabc3h@gmail.com>
2017-02-03 14:15:28 +0800

    ENUM triggered.

--- src/client_local.rs
+++ src/client_local.rs
@@ -1,51 +1,24 @@
-#[derive(Debug)]
+use std::io::Read;
+use std::fs::File;
+
 enum Body<'a> {
-    BufBody(&'a [u8], usize)
+    SizedBody(&'a mut (Read + 'a)),
+    BufBody,
 }
-#[derive(Debug)]
-struct Client {
-}
-
-impl Client {
-    fn new() -> Client {
-        Client{}
-    }
-    // fn get(self: &Client) -> Build {
-    fn get(&self) -> Build {
-        Build { client: self, body: None }
-    }
-}
-
+struct Client {}
 struct Build<'a> {
     client: &'a Client,
-    body: Option<Body<'a>>,
 }
-
 impl<'a> Build<'a> {
-    fn body(mut self, b: Body<'a>) -> Build<'a> {
-        self.body = Some(b);
-        // Build {body:None}
-        self
-    }
-
-    fn send(self) -> Result<i32,i32> {
-        println!("{:?}", self.client);
-        println!("{:?}", self.body);
-        Ok(5)
-    }
-}
-
-fn run () -> Result<i32,i32> {
-    let a = vec![1];
-    let b = Body::BufBody(&a,1);
-    let client = Client::new();
-    let mut builder = client.get();
-    builder = builder.body(b);
-    builder.send()
+    fn body(self, _: Body<'a>) {}
 }
 
 fn main() {
-    let x = run().unwrap();
-    println!("{:?}",x);
+    // let mut f = File::open("foo.txt").unwrap();
+    // let b = Body::SizedBody(&mut f);
+    let client = Client {};
+    let b = Body::BufBody;
+    let builder = Build { client: &client };
+    builder.body(b);
 }
 

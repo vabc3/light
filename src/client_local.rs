e9866eab74eca311a94d76aa9a91e16fc593a5e2
kar <vabc3h@gmail.com>
2017-02-03 15:04:39 +0800

    Step2

--- src/client_local.rs
+++ src/client_local.rs
@@ -2,23 +2,31 @@ use std::io::Read;
 use std::fs::File;
 
 enum Body<'a> {
-    SizedBody(&'a mut (Read + 'a)),
-    BufBody,
+    // SizedBody(&'a mut (Read + 'a)),
+    BufBody(&'a mut i32),
 }
 struct Client {}
 struct Build<'a> {
     client: &'a Client,
+    body: Option<Body<'a>>,
 }
 impl<'a> Build<'a> {
-    fn body(self, _: Body<'a>) {}
+    fn body(&mut self, a1: Body<'a>) {
+        self.body = Some(a1);
+    }
 }
 
 fn main() {
     // let mut f = File::open("foo.txt").unwrap();
     // let b = Body::SizedBody(&mut f);
+    let mut x = 7i32;
+    let b = Body::BufBody(&mut x);
     let client = Client {};
-    let b = Body::BufBody;
-    let builder = Build { client: &client };
-    builder.body(b);
+    let mut builder = Build {
+        client: &client,
+        body: None,
+    };
+    {
+        builder.body(b);
+    }
 }
-

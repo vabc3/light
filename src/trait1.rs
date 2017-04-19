09551e26e2dd4afb0a69d135c4056518561d4a37
kar <vabc3h@gmail.com>
2017-04-19 18:56:25 +0800

    Add trait1.

---
+++ src/trait1.rs
@@ -0,0 +1,19 @@
+// http://stackoverflow.com/questions/43493129/how-does-asref-work-with-str/43493257#43493257
+// good edit and answer.
+
+use std::path::Path;
+
+trait Trait1 {}
+
+impl Trait1 for str {}
+// impl<'a> Trait1 for &'a str {}
+impl<'a, T> Trait1 for &'a T where T: ?Sized + Trait1 {}
+
+fn run<T: Trait1>(_: T) {}
+fn run1<T: AsRef<Path>>(_: T) {}
+
+fn main() {
+    // E0277: the trait bound `&str: Trait1` is not satisfied
+    run::<&str>("sf");
+    run1::<&str>("sf");
+}

9f62043324d03ea351425d07edb7a4019b40c415
kar <vabc3h@gmail.com>
2017-05-09 13:44:10 +0800

    Add trait1.

--- src/trait1.rs
+++ src/trait1.rs
@@ -7,13 +7,13 @@ trait Trait1 {}
 
 impl Trait1 for str {}
 // impl<'a> Trait1 for &'a str {}
-impl<'a, T> Trait1 for &'a T where T: ?Sized + Trait1 {}
+// impl<'a, T> Trait1 for &'a T where T: ?Sized + Trait1 {}
 
-fn run<T: Trait1>(_: T) {}
+fn run<T: ?Sized + Trait1>(_: &T) {}
 fn run1<T: AsRef<Path>>(_: T) {}
 
 fn main() {
     // E0277: the trait bound `&str: Trait1` is not satisfied
-    run::<&str>("sf");
+    run::<str>("sf");
     run1::<&str>("sf");
 }
